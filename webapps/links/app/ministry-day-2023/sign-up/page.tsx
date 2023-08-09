2617a522550ff786a1591cc6c1baac6aee8f88c3
conradkoh <conradkoh@gmail.com>
2023-08-09 12:28:55 +0800

    added actual url for ministry day sign up redirect

--- webapps/links/app/ministry-day-2023/sign-up/page.tsx
+++ webapps/links/app/ministry-day-2023/sign-up/page.tsx
@@ -3,8 +3,8 @@
 import { useEffect } from 'react';
 
 export default function Page() {
-  //redirect to the landing page for the ministry day volunteer sign up form
   useEffect(() => {
-    window.location.href = 'https://www.google.com';
+    //redirect to the landing page for the ministry day volunteer sign up form
+    window.location.href = 'https://tinyurl.com/MOHRegister';
   }, []);
 }
