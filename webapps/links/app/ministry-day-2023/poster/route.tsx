a027616bd0627ecb7a1e7c7b5e6d2264a2eba2fc
conradkoh <conradkoh@gmail.com>
2023-08-15 00:56:32 +0800

    change poster redirect to actual site page

--- webapps/links/app/ministry-day-2023/poster/route.tsx
+++ webapps/links/app/ministry-day-2023/poster/route.tsx
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function GET() {
-  return NextResponse.redirect('https://tinyurl.com/MOHRegister');
+  return NextResponse.redirect(
+    'https://sites.google.com/view/bethel-av-open-house-2023/home'
+  );
 }
