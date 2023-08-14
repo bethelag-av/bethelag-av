e4a0fcb5ee0aaa534fd0ad6b3a30542c8cc88a56
conradkoh <conradkoh@gmail.com>
2023-08-15 00:46:12 +0800

    added redirect link for poster, added main index of links

--- webapps/links/app/page.tsx
+++ webapps/links/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <></>;
+  return (
+    <main className="p-5">
+      <article className="prose lg:prose-xl">
+        <h2>Links</h2>
+        <ul>
+          <li>
+            <a href="/ministry-day-2023/poster">Ministry Day Poster QR Code</a>
+          </li>
+          <li>
+            <a href="/ministry-day-2023/sign-up">
+              Ministry Day Sign Up QR Code
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
 }
