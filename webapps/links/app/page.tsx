d7bbdf4a4e368c9e7674c0fbccb6d2312f43654c
conradkoh <conradkoh@gmail.com>
2023-08-16 19:14:54 +0800

    added link for mandarin poster

--- webapps/links/app/page.tsx
+++ webapps/links/app/page.tsx
@@ -8,6 +8,11 @@ export default function Home() {
             <a href="/ministry-day-2023/poster">Ministry Day Poster QR Code</a>
           </li>
           <li>
+            <a href="/ministry-day-2023/poster/zh">
+              Ministry Day Poster QR Code (Mandarin)
+            </a>
+          </li>
+          <li>
             <a href="/ministry-day-2023/sign-up">
               Ministry Day Sign Up QR Code
             </a>
